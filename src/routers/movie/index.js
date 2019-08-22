771d66fbd3e862f7a271129a3e5cf211766880a1
codingories <tellt@126.com>
2019-08-22 16:11:46 +0800

    add createComponents

---
+++ src/routers/movie/index.js
@@ -0,0 +1,29 @@
+export default {
+    path : '/movie',
+    component : () => import('@/views/Movie'),
+    children : [
+        {
+            path : 'nowPlaying',
+            component : () => import('@/components/NowPlaying')
+        },
+        {
+            path : 'city',
+            component : () => import('@/components/City')
+        },
+        {
+            path : 'comingSoon',
+            component : () => import('@/components/ComingSoon')
+        },
+        {
+            path : 'search',
+            component : () => import('@/components/Search')
+        },
+        {
+            path : '/movie',
+            redirect : '/movie/nowPlaying'
+        }
+    ]
+
+
+}
+
